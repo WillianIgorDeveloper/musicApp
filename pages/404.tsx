26929bb263fa37643721e04c2ac9c92dfde304c7
Willian Igor <yellowmoonastronaut@gmail.com>
2022-09-11 18:11:39 -0300

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/notFound.module.scss'
+
+export default function NotFound () {
+    return(
+        <div className={styles.container}>
+            <h1>404 Not Found</h1>
+        </div>
+    )
+}
