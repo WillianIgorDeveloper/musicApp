00ddabfc910556f9c00dbe7ea868857f7239d64e
Willian Igor Santos <yellowmoonastronaut@gmail.com>
2022-09-11 18:33:29 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <meta name="description" content="Este é um app para reprodução de músicas feito com o intúito de aprendizagem." />
         {/* GoogleFonts: Roboto */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
       </Head>
       <body>
